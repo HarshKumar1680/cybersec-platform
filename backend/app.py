a520cc09584178f51622b7e9c25a63e5a5b3d88f
HarshKumar1680 <harshkumar1680@gmail.com>
2026-02-06 20:45:01 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
 from core_engine.nmap_scanner import NmapScanner
+from core_engine.owasp_scanner import OWASPScanner
 
 app = Flask(__name__)
 CORS(app)
 
-scanner = NmapScanner()
+nmap_scanner = NmapScanner()
+owasp_scanner = OWASPScanner()
+
 
 @app.route("/api/scan/network", methods=["POST"])
 def network_scan():
+    target = request.json.get("target")
+    result = nmap_scanner.scan_target(target)
+    return jsonify(result)
 
+
+@app.route("/api/scan/web", methods=["POST"])
+def web_scan():
     target = request.json.get("target")
 
-    result = scanner.scan_target(target)
+    vulnerabilities = owasp_scanner.run_scan(target)
 
-    return jsonify(result)
+    return jsonify({
+        "target": target,
+        "vulnerabilities": vulnerabilities
+    })
 
 
 if __name__ == "__main__":
